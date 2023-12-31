9c94be324eda3786a5119fce58ee618914d9a21b
Mayank Joshi <mayank.joshi@chekk.me>
2023-12-31 11:14:45 +0530

    Added Flutter application module and FastApi module

---
+++ app_blood_pressure_log/README.md
@@ -0,0 +1,3 @@
+# app_blood_pressure_log
+
+A
---
+++ app_blood_pressure_log/android/app/src/main/kotlin/com/mflutterapps/app_blood_pressure_log/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mflutterapps.app_blood_pressure_log
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
